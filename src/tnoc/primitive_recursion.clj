3c07c57dd3ed17762f0467f6694b03907d249973
SOberhoff <oberhoff.sebastian@gmail.com>
2017-07-06 11:35:27 +0200

    Added `force` and `delay` to primitive recursion. Changed all file separators to `\n`. Added primitive recursive `triangle` function.

--- src/tnoc/primitive_recursion.clj
+++ src/tnoc/primitive_recursion.clj
@@ -7,34 +7,41 @@
 
 (load "primitive_recursion_spec")
 
-(defn S-to-inc [body] (walk/prewalk-replace {'S 'inc} body))
+(defn delay-force [body]
+  (if-not (seq? body)
+    `(force ~body)
+    (->> body
+         (walk/postwalk
+           #(cond
+              (and (seq? %) (#{'dec 'inc} (first %))) `(~(first %) (force ~(second %)))
+              (seq? %) `(delay ~%)
+              :else %))
+         (list `force))))
+
+(defn S-to-inc [body] (walk/postwalk-replace {'S 'inc} body))
 
 (defn modify-recur [recur-params recur-body]
   (if (#{'_} (last recur-params))
-    [recur-params (S-to-inc recur-body)]
+    [recur-params recur-body]
     (let [modified-recur-params (spt/transform [spt/LAST] second recur-params)
           recur-param (second (last recur-params))
-          modified-recur-body (walk/postwalk-replace {recur-param (list 'dec recur-param)}
-                                                     (S-to-inc recur-body))]
+          modified-recur-body (walk/postwalk-replace {recur-param (list 'dec recur-param)} recur-body)]
       [modified-recur-params modified-recur-body])))
 
 (defmacro primrec
   ([name params body]
-   `(defn ~(vary-meta name assoc :primrec true) ~params ~(S-to-inc body)))
+   `(defn ~(vary-meta name assoc :primrec true) ~params ~(delay-force (S-to-inc body))))
   ([name base-params base-body recur-params recur-body]
-   (let [gen-params (into [] (repeatedly (count base-params) gensym))]
+   (let [gen-params (into [] (repeatedly (count base-params) gensym))
+         [modified-recur-params modified-recur-body] (modify-recur recur-params recur-body)]
      `(defn ~(vary-meta name assoc :primrec true) ~gen-params
-        (match ~gen-params
-               ~base-params ~(S-to-inc base-body)
-               ~@(modify-recur recur-params recur-body))))))
+        (match ~(spt/transform [spt/LAST] #(list `force %) gen-params)
+               ~base-params ~(delay-force (S-to-inc base-body))
+               ~modified-recur-params ~(delay-force (S-to-inc modified-recur-body)))))))
 
 (primrec add
          [x 0] x
          [x (S y)] (S (add x y)))
-
-(macroexpand-1 '(primrec add
-                         [x 0] x
-                         [x (S y)] (S (add x y))))
 
 (primrec mult
          [x 0] 0
@@ -83,3 +90,10 @@
          [x (S y)] (select (lg-rec x y) (sub x (S (S y))) (grt (exp (S (S 0)) (sub x (S y))) x)))
 
 (primrec lg [x] (lg-rec x x))
+
+(primrec triangle
+         [0] 0
+         [(S x)] (add (triangle x) (S x)))
+
+(primrec pair
+         [x y] (add (triangle (add x y)) y))
