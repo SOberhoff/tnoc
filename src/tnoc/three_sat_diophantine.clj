50a19b6bdf7e2ffdffd5b2c5af5e030f7c9eae00
SOberhoff <oberhoff.sebastian@gmail.com>
2017-07-10 14:07:45 +0200

    Finished setting up the equation.

--- src/tnoc/three_sat_diophantine.clj
+++ src/tnoc/three_sat_diophantine.clj
@@ -1,5 +1,6 @@
 (ns tnoc.three-sat-diophantine
-  (:require [clojure.core.match :refer [match]]
+  (:require [tnoc.subset-diophantine :refer [prime? merge-meshes]]
+            [clojure.core.match :refer [match]]
             [clojure.math.numeric-tower :refer [expt]]
             [clojure.spec :as spec]
             [clojure.spec.gen :as sgen]))
@@ -18,9 +19,6 @@
        (distinct)
        (filter (comp not #{'!}))))
 
-(defn compute-tau [formula]
-  (reduce + (map #(*' -1 (expt 8 %)) (range 1 (inc (count formula))))))
-
 (defn compute-fi's [formula]
   (->> formula
        (get-variables)
@@ -32,8 +30,49 @@
 (defn compute-n [formula] (+ (* 2 (count formula) (count (get-variables formula)))))
 
 (defn compute-cj's [formula]
-  (let [c0 1
-        c1-to-2m (->> (range 1 (inc (count formula)))
-                      (map #(if (even? %)
-                              (*' -1/2 (expt 8 (/ (dec %) 2)))
-                              (*' -1 (expt 8 (/ % 2))))))]))
+  (let [c1-to-2m (->> (range 1 (inc (* 2 (count formula))))
+                      (map #(if (odd? %)
+                              (*' -1/2 (expt 8 (/ (inc %) 2)))
+                              (*' -1 (expt 8 (/ % 2))))))
+        c2m+1-to-2m+l (->> (compute-fi's formula)
+                           (map #(*' 1/2 (-' (:fi+ %) (:fi- %)))))]
+    (concat [1] c1-to-2m c2m+1-to-2m+l)))
+
+(defn compute-tau [formula]
+  (let [tau-phi (reduce +' (map #(*' -1 (expt 8 %)) (range 1 (inc (count formula)))))
+        cj-sum (reduce +' (compute-cj's formula))
+        fi--sum (reduce +' (map :fi- (compute-fi's formula)))]
+    (+' tau-phi cj-sum fi--sum)))
+
+(def primes-greater-12 (filter prime? (iterate inc 13)))
+
+(defn compute-thetas [formula]
+  (let [cj's (compute-cj's formula)
+        m (count formula)
+        n (count cj's)
+        pj's (take m primes-greater-12)]
+    (->> (map vector cj's pj's)
+         (map (fn [[cj pj]]
+                (->> (range 1 pj)
+                     (map (fn [remainder]
+                            (reduce merge-meshes
+                                    [[(expt 8 (inc m)) cj]
+                                     [(reduce *' (map #(expt % (inc n)) (remove #{pj} pj's))) 0]
+                                     [pj remainder]])))
+                     (map second)
+                     (apply min)))))))
+
+(defn setup-equation [formula]
+  (let [thetas (compute-thetas formula)
+        m (count formula)
+        n (count thetas)
+        H (reduce +' thetas)
+        K (reduce *' (map #(expt % (inc n)) (take m primes-greater-12)))
+        tau (compute-tau formula)]
+    `(~'fn [~'x ~'y] (~'= (~'+'
+                            (~'*'
+                              ~(*' (expt (inc K) 3) 2 (expt 8 (inc m)))
+                              (~'-' ~(*' H H) (~'*' ~'x ~'x)))
+                            (~'*' ~K (~'-' (~'*' ~'x ~'x) ~(*' tau tau)))
+                            (~'*' ~(*' -2 (expt 8 (inc m)) K) ~'y))
+                       0))))
