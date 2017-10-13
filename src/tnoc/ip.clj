8e2d13e753596071ba7ae7babaaf7e39e01c76f3
SOberhoff <oberhoff.sebastian@gmail.com>
2017-10-13 16:06:33 -0500

    Wrote functions to arithmetize quantified boolean formulas.

--- src/tnoc/ip.clj
+++ src/tnoc/ip.clj
@@ -1,5 +1,7 @@
 (ns tnoc.ip
-  (:require [clojure.spec.alpha :as spec]))
+  (:require [clojure.spec.alpha :as spec]
+            [clojure.walk :as walk]
+            [clojure.math.numeric-tower :refer [expt]]))
 
 (load "ip_spec")
 
@@ -9,14 +11,16 @@
        (map second)
        (reduce +)))
 
-(defn simplify-term [[coefficient & variables]]
-  (->> variables
-       (reduce (fn [base->powers [base power]]
-                 (assoc base->powers base (+ (base->powers base 0) power)))
-               {})
-       (seq)
-       (sort-by first)
-       (concat [coefficient])))
+(defn simplify-term [term]
+  (loop [coefficient 1
+         base->powers {}
+         [next-factor & remaining-factors] term]
+    (if next-factor
+      (if (integer? next-factor)
+        (recur (* coefficient next-factor) base->powers remaining-factors)
+        (let [[base power] next-factor]
+          (recur coefficient (assoc base->powers base (+ (base->powers base 0) power)) remaining-factors)))
+      (concat [coefficient] (sort-by first (seq base->powers))))))
 
 (defn simplify-sum [terms]
   (->> (map simplify-term terms)
@@ -24,8 +28,14 @@
                  (assoc factors->coefficient variables (+ (factors->coefficient variables 0) coefficient)))
                {})
        (map #(concat [(second %)] (first %)))
+       (filter (comp not zero? first))
        (sort-by get-degree)
        (reverse)))
+
+(defn add-sums [sum-0 sum-1]
+  (if (spec/valid? ::term sum-1)
+    (concat sum-0 [sum-1])
+    (concat sum-0 (rest sum-1))))
 
 (defn multiply-sums [sum-0 sum-1]
   (for [[coefficient-0 & variables-0] sum-0
@@ -37,10 +47,9 @@
     (spec/valid? ::term polynomial) (simplify-term polynomial)
     (spec/valid? ::sum polynomial) (->> (rest polynomial)
                                         (map simplify-polynomial)
-                                        (reduce #(if (spec/valid? ::term %2)
-                                                   (concat %1 [%2])
-                                                   (concat %1 (rest %2)))
-                                                ['+]))
+                                        (reduce add-sums ())
+                                        (simplify-sum)
+                                        (concat ['+]))
     (spec/valid? ::product polynomial) (->> (rest polynomial)
                                             (map simplify-polynomial)
                                             (map #(if (spec/valid? ::term %) [%] (rest %)))
@@ -48,7 +57,42 @@
                                             (simplify-sum)
                                             (concat ['+]))))
 
+(defn substitute [polynomial substitution]
+  (walk/postwalk #(if (and (spec/valid? ::variable-power %) (contains? substitution (first %)))
+                    (expt (substitution (first %)) (second %))
+                    %)
+                 polynomial))
 
-(def t `(~'* (~'+ ~[2 [:x 2]] ~[5]) (~'+ ~[3 [:x 5]] ~[2 [:x 1]])))
+(defn arithmetic-negate [polynomial]
+  ['+ [1] ['* [-1] polynomial]])
 
-(simplify-polynomial t)
+(defn arithmetize [formula]
+  (cond
+    (spec/valid? ::literal formula)
+    (case formula false [0] true [1] [1 [formula 1]])
+
+    (spec/valid? ::negation formula)
+    (arithmetic-negate (arithmetize (second formula)))
+
+    (spec/valid? ::conjunction formula)
+    (into ['*] (map arithmetize (rest formula)))
+
+    (spec/valid? ::disjunction formula)
+    (->> (rest formula)
+         (map (comp arithmetic-negate arithmetize))
+         (into ['*])
+         (arithmetic-negate))
+
+    (spec/valid? ::all formula)
+    (let [[_ variable body] formula]
+      ['*
+       (arithmetize (walk/postwalk-replace {variable false} body))
+       (arithmetize (walk/postwalk-replace {variable true} body))])
+
+    (spec/valid? ::exists formula)
+    (let [[_ variable body] formula]
+      (arithmetic-negate
+        ['*
+         (arithmetic-negate (arithmetize (walk/postwalk-replace {variable false} body)))
+         (arithmetic-negate (arithmetize (walk/postwalk-replace {variable true} body)))]))))
+
--- src/tnoc/ip_spec.clj
+++ src/tnoc/ip_spec.clj
@@ -1,8 +1,8 @@
 (in-ns 'tnoc.ip)
 
-(spec/def ::variable (spec/cat :base keyword? :power nat-int?))
+(spec/def ::variable-power (spec/tuple keyword? nat-int?))
 
-(spec/def ::term (spec/cat :coefficient integer? :variable (spec/* (spec/spec ::variable))))
+(spec/def ::term (spec/* (spec/or :coefficient integer? :variable-power ::variable-power)))
 
 (spec/def ::sum (spec/cat :mult #{'+} :operands (spec/* ::polynomial)))
 
@@ -11,3 +11,23 @@
 (spec/def ::polynomial (spec/or :term ::term
                                 :sum ::sum
                                 :product ::product))
+
+(spec/def ::literal (spec/or :constant boolean? :variable keyword?))
+
+(spec/def ::negation (spec/cat :not #{'not} :clause ::formula))
+
+(spec/def ::conjunction (spec/cat :and #{'and} :clauses (spec/* ::formula)))
+
+(spec/def ::disjunction (spec/cat :or #{'or} :clauses (spec/* ::formula)))
+
+(spec/def ::all (spec/cat :A #{'A} :variable keyword? :body ::formula))
+
+(spec/def ::exists (spec/cat :E #{'E} :variable keyword? :body ::formula))
+
+(spec/def ::formula (spec/or
+                      :literal ::literal
+                      :negation ::negation
+                      :conjuction ::conjunction
+                      :disjuction ::disjunction
+                      :all ::all
+                      :exists ::exists))
