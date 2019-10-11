1e6595ac69cc0aa878e2a4cdb064a5d983dab8be
oberhoff <oberhoff.sebastian@gmail.com>
2019-10-10 21:32:19 -0500

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/SOberhoff/tnoc"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0-RC3"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.match "0.3.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/test.check "0.9.0"]
-                 [com.rpl/specter "1.1.1"]]
+                 [org.clojure/test.check "0.10.0"]
+                 [com.rpl/specter "1.1.2"]]
   :jvm-opts ["-Xss64m"])
 
